c6d5e6c43d7363151ebc5ace2a3cc691471f8a69
dcgaines <dcgaines@mtu.edu>
2018-11-12 16:43:16 -0500

    Finished problem 2 and updated includes in template

--- 002/002.c
+++ 002/002.c
@@ -1,8 +1,19 @@
-#include <stdlib>
-#include <stdio>
+#include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
 int main(int argc, char** argv) {
+  long one = 1;
+  long two = 1;
+  long temp, sum = 0;
 
+  do {
+    temp = one + two;
+    if(temp % 2 == 0)
+      sum += temp;
+    one = two;
+    two = temp;
+  } while (temp < 4000000);
+  printf("%ld\n", sum);
   return 0;
 }
